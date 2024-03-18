f53046327a143d5d9a3f221f73dbfa30c834e2b8
Nate Rivard <nrivard@outlook.com>
2024-03-18 16:41:48 +0000

    Added basic MFP proof-of-concept

--- src/rom/startup.s
+++ src/rom/startup.s
@@ -1,55 +1,80 @@
     section text
 
-    public Start
+    public  Start
 
-Vectors:
-    dc.l    STACK_START                 ; $00: reset SP
-    dc.l    Start                       ; $01: reset PC
-    dc.l    HandleBusError              ; $02: bus error handler
-    dc.l    HandleAddrError             ; $03: misaligned memory access
-    dc.l    HandleIllegalInstr          ; $04: illegal instruction handler
-    dc.l    HandleGenericInterrupt      ; $05: divide by zero
-    dc.l    HandleGenericInterrupt      ; $06: CHK instr resulted in out of bounds
-    dc.l    HandleGenericInterrupt      ; $07: TRAPV instr
-    dc.l    HandleGenericInterrupt      ; $08: privilege violation
-    dc.l    HandleGenericInterrupt      ; $09: trace mode
-    dc.l    HandleGenericInterrupt      ; $0A: 1010 (line $A) emulator
-    dc.l    HandleGenericInterrupt      ; $0B: 1111 (line $F) emulator
-    dcb.l   3,HandleGenericInterrupt    ; $0C-0E: Reserved
-    dc.l    HandleGenericInterrupt      ; $0F: uninitialized interrupt vector
-VectorsEnd:
-VectorCount: equ (VectorsEnd-Vectors)/4 ; 256 longwords
+MFP_BASE    equ $E00001
+MFP_GPIP    equ MFP_BASE+($00<<1)
+MFP_DDR     equ MFP_BASE+($02<<1)
+MFP_TCDCR   equ MFP_BASE+($0E<<1)
+MFP_TCDR    equ MFP_BASE+($11<<1)
+MFP_TDDR    equ MFP_BASE+($12<<1)
+MFP_UCR     equ MFP_BASE+($14<<1)
+MFP_RSR     equ MFP_BASE+($15<<1)
+MFP_TSR     equ MFP_BASE+($16<<1)
+MFP_UDR     equ MFP_BASE+($17<<1)
 
-    dcb.l   8,HandleGenericInterrupt    ; $10-17: Reserved
-    dc.l    HandleDebug                 ; $18: spurious interrupt
-    dcb.l   7,HandleGenericInterrupt    ; $19-1F: autovectors
-    
-    dcb.l   13,HandleGenericInterrupt   ; $20-2C: unused Trap handlers
-    dc.l    HandleGenericInterrupt      ; $2D: trap#13
-    dc.l    HandleTrap14                ; $2E: trap#14
-    dc.l    HandleGenericInterrupt      ; $2F: trap#15
+MFP_TIMER_DELAY_PRESCALE_4      equ     %001
+MFP_UCR_DIVIDE_BY_16            equ     (%1<<7)
+MFP_UCR_STOP_BIT_1              equ     (%01<<3)
+MFP_RSR_BUFFER_FULL             equ     (%1<<7)
+MFP_RSR_ENABLE                  equ     (%1)
+MFP_TSR_BUFFER_EMPTY            equ     (%1<<7)
+MFP_TSR_SET_HIGH                equ     (%1<<2)
+MFP_TSR_ENABLE                  equ     (%1)
 
-    dcb.l   16,HandleGenericInterrupt   ; $30-3F reserved
-    dcb.l   $C0,HandleGenericInterrupt  ; $40-FF user vectors
+    org     $400000
+
+    dc.l    $100000
+    dc.l    Start
+    dcb.l   254,Error
 
 Start:
-    lea.l   Vectors,A0                  ; copy vectors into RAM (overlay should have triggered)
-    lea.l   $000000,A1
-    move.w  #(VectorCount-1),D0 ; number of longwords to copy (should be $100...)
-.VectorLoop:
-    move.l  (A0)+,(A1)+                 ; copy contents pointed at by A0 into A1 and post-increment both pointers
-    dbra    D0,.VectorLoop
-    
-HandleBusError:
+    jsr MFPInit
+.Echo:
+    jsr MFPReceive
+    jsr MFPSend
+    bchg #(7),MFP_GPIP      ; toggle LED bit
+    bra.s .Echo
 
-HandleAddrError:
+MFPInit:
+    move.b  #$FE,MFP_DDR                                        ; set all but bit 0 of GPIO as outputs
+.Timer:
+    move.b  #$03,MFP_TDDR                                       ;         CLK     | prescale | UCR bit 7 | timer value | RC toggle
+    move.b  #(MFP_TIMER_DELAY_PRESCALE_4),MFP_TCDCR             ; 9600 = (3686400 / 4        / 16        / 3           / 2)
+.UART:
+    move.b  #(MFP_UCR_DIVIDE_BY_16|MFP_UCR_STOP_BIT_1),MFP_UCR  ; divide-by 16, async, 8N1
+    move.b  #(MFP_TSR_SET_HIGH|MFP_TSR_ENABLE),MFP_TSR          ; set line default high, enable transmitter
+    move.b  #(MFP_RSR_ENABLE),MFP_RSR                           ; enable receiver
+    rts
 
-HandleIllegalInstr:
+; Sends char in D0
+MFPSend:
+    tst.b   MFP_TSR     ; ready to send?
+    bpl     MFPSend
+.Send:
+    move.b  D0,MFP_UDR
+.Done:
+    rts
 
-HandleGenericInterrupt:
+; Receives char in D0
+MFPReceive:
+    tst.b   MFP_RSR     ; ready to receive?
+    bpl     MFPReceive
+.Recv:
+    move.b  MFP_UDR,D0
+.Done:
+    rts
 
-HandleDebug:
+; ; sends null-terminated string in A0
+; ; trashes D0, MFP_UDR
+; ; modifies A0 (points to char after null-terminator)
+; MFPSendString:
+;     move.b  (A0)+,D0
+;     beq.s   .Done           ; null-terminator?
 
-HandleTrap14:
-    
-STACK_START:
+; .Done:
+;     rts
+
+; WelcomeMsg: dc.b   "Can anyone hear me?", 0
+
+Error:
