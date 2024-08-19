ba0a7221fa889bc8c3a08fa94f0848c2c4c39937
= <nrivard@outlook.com>
2024-08-19 17:40:43 +0200

    last bit of prep before tearing a lot of this down into a more barebones loader

--- src/rom/trap13.s
+++ src/rom/trap13.s
@@ -9,30 +9,18 @@ HANDLETRAP13::
     add.l   D0,D0
     add.l   D0,D0                       ; adding twice is faster than shifting or mul
     move.l  .FunctionTable(PC,D0),A0    ; get offset into function table
-    jmp     (A0)
+    jsr     (A0)
 .Done:
     rte
 
 .FunctionTable:
-    dc.l    .SPI_INIT
-    dc.l    .SPI_ASSERT
-    dc.l    .SPI_DEASSERT
-    dc.l    .SPI_TX
-    ; dc.l    SDCARDINIT
-    ; dc.l    SDCARDREADBLK
+    dc.l    SDCARDINIT                  ; call into direct
+    dc.l    .SDCARD_RD_BLK
+    dc.l    SPIINIT                     ; call into direct (should be removed!)
+    dc.l    SPIASSERT                   ; call into direct (should be removed!)
+    dc.l    SPIDEASSERT                 ; call into direct (should be removed!)
+    dc.l    .SPI_TX                     ; should be removed!
 .FunctionTableEnd:
-
-.SPI_INIT:
-    jsr     SPIINIT
-    rts
-
-.SPI_ASSERT:
-    jsr     SPIASSERT
-    rts
-
-.SPI_DEASSERT:
-    jsr     SPIDEASSERT
-    rts
 
 ; D1 should contain the byte to transfer
 ; on return, D0 will contain the return value
@@ -40,3 +28,7 @@ HANDLETRAP13::
     move.l  D1,D0
     jsr     SPITX
     rts
+
+; D1 should contain the block number to read
+.SDCARD_RD_BLK:
+    move.l D1,D0
